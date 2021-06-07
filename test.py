da23c5ca2e84b8a114de5d35d0a1c463dac04034
FrostbiteXSW <33869074+FrostbiteXSW@users.noreply.github.com>
2021-06-07 15:03:24 +0800

    test() will return models and datasets.

--- test.py
+++ test.py
@@ -107,16 +107,18 @@ def test(
 		kmeans_pri.save_model(snapshot_kmeans_pri)
 		kmeans_pos.save_model(snapshot_kmeans_pos)
 
+	return model, kmeans_pri, kmeans_pos, trainset, testset
+
 
 if __name__ == '__main__':
-	config = Configs.config_gtsrb
+	config = Configs.config_mnist
 	snapshot = './checkpoints/{}/model.ckpt'.format(config['dataset'])
 	snapshot_kmeans_pri = './checkpoints/{}/kmeans_pri/model.ckpt'.format(config['dataset'])
 	snapshot_kmeans_pos = './checkpoints/{}/kmeans_pos/model.ckpt'.format(config['dataset'])
 	train_and_save_kmeans = not (os.path.exists(snapshot_kmeans_pri[:snapshot_kmeans_pri.rindex('/')])
 	                             and os.path.exists(snapshot_kmeans_pos[:snapshot_kmeans_pos.rindex('/')]))
 
-	test(
+	model, kmeans_pri, kmeans_pos, trainset, testset = test(
 		config=config,
 		snapshot=snapshot,
 		snapshot_kmeans_pri=snapshot_kmeans_pri,
